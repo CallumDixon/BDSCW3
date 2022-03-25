3515132a5fa5c8ce362c5e6b636866fa4efdb364
Callum Dixon <callum.dixon002@gmail.com>
2022-03-25 11:36:28 +0000

    bug fix

--- run.sh
+++ run.sh
@@ -2,8 +2,14 @@
 declare -a arr1=("5" "30" "50") #iterations
 declare -a arr2=("3" "5" "10") #children
 declare -a arr3=("periodically" "convergence" "globalCostReduction" "never") #stratergy
-declare -a arr4=("0.01 0.2 0.5") #alpha
-declare -a arr5=("0.01 0.2 0.5") #beta
+declare -a arr4=("0.01" "0.2" "0.5") #alpha
+declare -a arr5=("0.01" "0.2" "0.5") #beta
+
+# declare -a arr1=("5" "30") #iterations
+# declare -a arr2=("3" "5") #children
+# declare -a arr3=("periodically") #stratergy
+# declare -a arr4=("0.01") #alpha
+# declare -a arr5=("0.01") #beta
 
 for i in "${arr1[@]}"
 do
