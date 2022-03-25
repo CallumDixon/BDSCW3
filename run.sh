385a7d32ca838354e4e461ae800f2bf54fcde72f
Callum Dixon <callum.dixon002@gmail.com>
2022-03-25 11:23:41 +0000

    added updated params

--- run.sh
+++ run.sh
@@ -1,13 +1,25 @@
 
 declare -a arr1=("5" "30" "50") #iterations
 declare -a arr2=("3" "5" "10") #children
+declare -a arr3=("periodically" "convergence" "globalCostReduction" "never") #stratergy
+declare -a arr4=("0.01 0.2 0.5") #alpha
+declare -a arr5=("0.01 0.2 0.5") #beta
 
 for i in "${arr1[@]}"
 do
     for j in "${arr2[@]}"
+    do
+        for k in "${arr3[@]}"
         do
-            echo "run"
-            python3 config.py $i $j
-            java -jar IEPOS-Tutorial.jar
+            for l in "${arr4[@]}"
+            do
+                for m in "${arr5[@]}"
+                do
+                    echo "run"
+                    python3 config.py $i $j $k $l $m
+                    java -jar IEPOS-Tutorial.jar
+                done
+            done
         done
+    done
 done
